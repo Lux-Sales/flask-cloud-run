52e6be8b682563505227f9bb1fe49c2b46b53471
Lucas <sales_l@hotmail.com>
2022-09-08 13:40:11 -0300

    initial commit

--- deploy-appengine.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-gcloud app deploy alunos/app.yaml --quiet \
-&& gcloud app deploy materias/app.yaml --quiet \
-&& gcloud app deploy default/app.yaml --quiet \
-&& gcloud app deploy dispatch.yaml --quiet
